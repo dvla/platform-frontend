397f4a3052ff98cbccea9e8e7e68645e82ae77b9
Chris Williams <Chris.Williams@dvla.gov.uk>
2020-01-05 20:46:10 +0000

    styling on new kubernetes secret

--- src/views/secrets/NewSecret.js
+++ src/views/secrets/NewSecret.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import SecretForm from './SecretForm';
 
 export default class NewSecret extends Component {
@@ -9,9 +12,22 @@ export default class NewSecret extends Component {
       <>
         <div className="container-fluid">
           <h1 data-testid="title" className="h3 mb-4 text-gray-800">
-            New Secret
+            Kubernetes Secrets
           </h1>
-          <SecretForm />
+          <Card className="shadow mb-4">
+            <Card.Header className="py-3">
+              <h6 className="m-0 font-weight-bold text-primary">
+                New Kubernetes Secret
+              </h6>
+            </Card.Header>
+            <Card.Body>
+              <Row>
+                <Col>
+                  <SecretForm />
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
         </div>
       </>
     );
