d0db45728bae43b2abd65169a659e0a3bcef1c5c
Chris Williams <Chris.Williams@dvla.gov.uk>
2020-01-06 15:39:44 +0000

    banner messages on secrets

--- src/views/secrets/SecretForm.js
+++ src/views/secrets/SecretForm.js
@@ -6,6 +6,7 @@ import { Formik } from 'formik';
 import Button from 'react-bootstrap/Button';
 import * as yup from 'yup'; // for everything
 import Col from 'react-bootstrap/Col';
+import DVLAAlert from '../../components/Alert';
 
 export default class SecretForm extends Component {
   constructor(props) {
@@ -13,19 +14,43 @@ export default class SecretForm extends Component {
     this.state = {
       name: props.nameProps || '',
       value: '',
-      description: ''
+      description: '',
+      message: null,
+      status: null
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  handleSubmit(data) {
+  async handleSubmit(data) {
     const apiName = 'backend';
     const path = 'ssm/postParameter';
-    return API.post(apiName, path, { body: data });
+
+    let response = [];
+    try {
+      response = await API.post(apiName, path, { body: data });
+      this.setState({
+        message: 'Kubernetes Secret Updated',
+        status: 'success'
+      });
+    } catch (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        this.setState({ message: error.message, status: 'danger' });
+      } else if (error.request) {
+        this.setState({ message: 'Authentication Denied', status: 'danger' });
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        this.setState({ message: error.message, status: 'danger' });
+      }
+    }
+
+    return response;
   }
 
   render() {
-    const { value, description, name } = this.state;
+    const { value, description, name, message, status } = this.state;
+
     const { nameProps } = this.props;
     const schema = yup.object({
       name: yup
@@ -39,90 +64,94 @@ export default class SecretForm extends Component {
       description: yup.string().required()
     });
     return (
-      <Formik
-        validationSchema={schema}
-        onSubmit={this.handleSubmit}
-        initialValues={{
-          name,
-          value,
-          description
-        }}
-      >
-        {({
-          handleSubmit,
-          handleChange,
-          handleBlur,
-          values,
-          touched,
-          errors
-        }) => (
-          <Form noValidate onSubmit={handleSubmit}>
-            <Form.Row>
-              <Form.Group as={Col} md="12" controlId="name">
-                <Form.Label>Name</Form.Label>
-                <InputGroup>
-                  <Form.Control
-                    type="text"
-                    name="name"
-                    readOnly={nameProps}
-                    value={values.name}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    isInvalid={!!touched.name && !!errors.name}
-                  />
-                  <Form.Control.Feedback
-                    data-testid="errors-name"
-                    type="invalid"
-                  >
-                    {errors.name}
-                  </Form.Control.Feedback>
-                </InputGroup>
-              </Form.Group>
+      <>
+        {message && <DVLAAlert message={message} status={status} />}
 
-              <Form.Group as={Col} md="12" controlId="value">
-                <Form.Label>Value</Form.Label>
-                <InputGroup>
-                  <Form.Control
-                    type="text"
-                    name="value"
-                    value={values.value}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    isInvalid={!!touched.value && !!errors.value}
-                  />
-                  <Form.Control.Feedback
-                    data-testid="errors-value"
-                    type="invalid"
-                  >
-                    {errors.value}
-                  </Form.Control.Feedback>
-                </InputGroup>
-              </Form.Group>
+        <Formik
+          validationSchema={schema}
+          onSubmit={this.handleSubmit}
+          initialValues={{
+            name,
+            value,
+            description
+          }}
+        >
+          {({
+            handleSubmit,
+            handleChange,
+            handleBlur,
+            values,
+            touched,
+            errors
+          }) => (
+            <Form noValidate onSubmit={handleSubmit}>
+              <Form.Row>
+                <Form.Group as={Col} md="12" controlId="name">
+                  <Form.Label>Name</Form.Label>
+                  <InputGroup>
+                    <Form.Control
+                      type="text"
+                      name="name"
+                      readOnly={nameProps}
+                      value={values.name}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      isInvalid={!!touched.name && !!errors.name}
+                    />
+                    <Form.Control.Feedback
+                      data-testid="errors-name"
+                      type="invalid"
+                    >
+                      {errors.name}
+                    </Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
 
-              <Form.Group as={Col} md="12" controlId="description">
-                <Form.Label>Description</Form.Label>
-                <InputGroup>
-                  <Form.Control
-                    type="text"
-                    name="description"
-                    value={values.description}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    isInvalid={!!touched.description && !!errors.description}
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    {errors.description}
-                  </Form.Control.Feedback>
-                </InputGroup>
-              </Form.Group>
-            </Form.Row>
+                <Form.Group as={Col} md="12" controlId="value">
+                  <Form.Label>Value</Form.Label>
+                  <InputGroup>
+                    <Form.Control
+                      type="text"
+                      name="value"
+                      value={values.value}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      isInvalid={!!touched.value && !!errors.value}
+                    />
+                    <Form.Control.Feedback
+                      data-testid="errors-value"
+                      type="invalid"
+                    >
+                      {errors.value}
+                    </Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
 
-            <Button id="submit" type="submit" variant="primary">
-              Update
-            </Button>
-          </Form>
-        )}
-      </Formik>
+                <Form.Group as={Col} md="12" controlId="description">
+                  <Form.Label>Description</Form.Label>
+                  <InputGroup>
+                    <Form.Control
+                      type="text"
+                      name="description"
+                      value={values.description}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      isInvalid={!!touched.description && !!errors.description}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.description}
+                    </Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
+              </Form.Row>
+
+              <Button id="submit" type="submit" variant="primary">
+                Update
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </>
     );
   }
 }
