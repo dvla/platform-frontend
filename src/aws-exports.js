ef0de742d78df676c6e877d33e7f58b6869619fc
Chris Williams <Chris.Williams@dvla.gov.uk>
2020-01-06 09:03:13 +0000

    support for dev and production

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,4 +1,29 @@
-const awsmobile = {
+const dev = {
+  Auth: {
+    identityPoolId: 'eu-west-2:06c92ab8-3994-408a-8cd7-3f7a85669d40',
+    region: 'eu-west-2',
+    userPoolId: 'eu-west-2_Cu3HQIN1H',
+    userPoolWebClientId: '1v4i4f7pourkoi4pnm2v7cu529',
+    oauth: {
+      domain: 'int-platform.auth.eu-west-2.amazoncognito.com',
+      scope: ['phone', 'email', 'profile'],
+      redirectSignIn: 'http://localhost:3000',
+      redirectSignOut: 'http://localhost:3000',
+      responseType: 'code'
+    }
+  },
+  API: {
+    endpoints: [
+      {
+        name: 'backend',
+        region: 'eu-west-2',
+        endpoint: 'https://6smehzhm0a.execute-api.eu-west-2.amazonaws.com/int/'
+      }
+    ]
+  }
+};
+
+const prod = {
   Auth: {
     identityPoolId: 'eu-west-2:06c92ab8-3994-408a-8cd7-3f7a85669d40',
     region: 'eu-west-2',
@@ -22,5 +47,7 @@ const awsmobile = {
     ]
   }
 };
+
+const awsmobile = process.env.REACT_APP_STAGE === 'production' ? prod : dev;
 
 export default awsmobile;
