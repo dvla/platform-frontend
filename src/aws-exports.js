74e8deaca33f9e6a8720aca2e2a750531f144ba9
Chris Williams <chris.williams@dvla.gov.uk>
2020-01-25 12:05:58 +0000

    update endpoint in tooling account

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,11 +1,11 @@
 const dev = {
   Auth: {
-    identityPoolId: 'eu-west-2:06c92ab8-3994-408a-8cd7-3f7a85669d40',
+    identityPoolId: 'eu-west-2:f6bc2a55-25f4-4844-b308-47afcd12a577',
     region: 'eu-west-2',
-    userPoolId: 'eu-west-2_Cu3HQIN1H',
-    userPoolWebClientId: '1v4i4f7pourkoi4pnm2v7cu529',
+    userPoolId: 'eu-west-2_0W6smmkSs',
+    userPoolWebClientId: '3307n637gdg6ood5jvd1n8btpq',
     oauth: {
-      domain: 'int-platform.auth.eu-west-2.amazoncognito.com',
+      domain: 'auth-ss-platform.auth.eu-west-2.amazoncognito.com',
       scope: ['phone', 'email', 'profile'],
       redirectSignIn: 'http://localhost:3000',
       redirectSignOut: 'http://localhost:3000',
@@ -17,7 +17,8 @@ const dev = {
       {
         name: 'backend',
         region: 'eu-west-2',
-        endpoint: 'https://6smehzhm0a.execute-api.eu-west-2.amazonaws.com/int/'
+        endpoint:
+          'https://jd2ms9t2v1.execute-api.eu-west-2.amazonaws.com/auth/nonprod/'
       }
     ]
   }
@@ -25,12 +26,12 @@ const dev = {
 
 const prod = {
   Auth: {
-    identityPoolId: 'eu-west-2:06c92ab8-3994-408a-8cd7-3f7a85669d40',
+    identityPoolId: 'eu-west-2:f6bc2a55-25f4-4844-b308-47afcd12a577',
     region: 'eu-west-2',
-    userPoolId: 'eu-west-2_Cu3HQIN1H',
-    userPoolWebClientId: '1v4i4f7pourkoi4pnm2v7cu529',
+    userPoolId: 'eu-west-2_0W6smmkSs',
+    userPoolWebClientId: '3307n637gdg6ood5jvd1n8btpq',
     oauth: {
-      domain: 'int-platform.auth.eu-west-2.amazoncognito.com',
+      domain: 'auth-ss-platform.auth.eu-west-2.amazoncognito.com',
       scope: ['phone', 'email', 'profile'],
       redirectSignIn: 'https://platform.tooling.dvla.gov.uk',
       redirectSignOut: 'https://platform.tooling.dvla.gov.uk',
@@ -42,7 +43,8 @@ const prod = {
       {
         name: 'backend',
         region: 'eu-west-2',
-        endpoint: 'https://6smehzhm0a.execute-api.eu-west-2.amazonaws.com/int/'
+        endpoint:
+          'https://jd2ms9t2v1.execute-api.eu-west-2.amazonaws.com/auth/nonprod/'
       }
     ]
   }
